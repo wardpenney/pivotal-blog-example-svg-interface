b1e9597f21e5eefd64b9fcd62a5b20d5a69ff99e
Ward Penney <pair+wardpenney@pivotallabs.com>
2013-10-28 09:47:16 -0400

    Get d3 basics going

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,2 +1,25 @@
-//= require_tree .
+//= require vendor/d3.v3
+//= require_self
 
+//
+
+(function() {
+  d3.selectAll(".tall-block")
+    .data([1,2,3,4,5])
+    .enter().append("p.tall-block")
+      .text(function(d) { return "I’m number " + d + "!"; });
+
+  // var svg = d3.select("#chart-2").append("svg")
+      // .attr("width", w)
+      // .attr("height", h);
+
+  // var circle = svg.selectAll("circle")
+      // .data([32, 57, 293], String);
+
+  // circle.enter().append("circle")
+      // .attr("cy", 90)
+      // .attr("cx", String)
+      // .attr("r", Math.sqrt);
+
+  // circle.exit().remove();
+});
